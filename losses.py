7ed527f2e51ad1da8bcc04cd2d1e2301a4dc9a2d
TNK2718 <mizuki2718@ruri.waseda.jp>
2022-12-02 21:43:45 +0900

    small fix

--- losses.py
+++ losses.py
@@ -4,11 +4,11 @@ import torch
 def sphere_levelset_loss(model, input):
     pred, _ = model(input)
     gt_norm = torch.linalg.norm(input, dim=1, keepdim=True)
-    gt = gt_norm# - torch.ones_like(gt_norm)
+    gt = gt_norm - torch.ones_like(gt_norm)
     loss = torch.mean(torch.abs(pred - gt))
     return loss
 
 # Debug
 def plane_loss(model, input):
     pred, _ = model(input)
-    return torch.mean(torch.abs(pred - torch.ones_like(pred)))
+    return torch.mean(torch.abs(pred))
